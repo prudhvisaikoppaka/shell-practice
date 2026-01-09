89d9c5a1e667dcc93add57c72bc8d27c1057cfa7
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-09 16:33:33 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Prudhvi:: Hi NTR Anna, How are you?"
+echo "NTR::     Hi ra, I am Good. How are you doing"
+echo "Prudhvi:: I am fine too. What about Dragon?"
+echo "NTR::     Dragon coming excellent."
+echo "Prudhvi:: Can't wait to see Dragon anna."
+       
