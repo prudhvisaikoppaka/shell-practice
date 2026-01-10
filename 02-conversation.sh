c70ab3edb9b2acb328434ec370219899480d3ea4
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-10 12:18:42 +0530

    happy Sankranthi

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -5,4 +5,3 @@ echo "NTR::     Hi ra, I am Good. How are you doing"
 echo "Prudhvi:: I am fine too. What about Dragon?"
 echo "NTR::     Dragon coming excellent."
 echo "Prudhvi:: Can't wait to see Dragon anna."
-       
---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1 = $PERSON1
+PERSON2 = $PERSON2
+
+echo "$PERSON1:: Hi $PERSON2 Anna, How are you?"
+echo "$PERSON2:: Hi ra, I am Good. How are you doing"
+echo "$PERSON1:: I am fine too. What about Dragon?"
+echo "$PERSON2:: Dragon coming excellent."
+echo "$PERSON1:: Can't wait to see Dragon anna."
