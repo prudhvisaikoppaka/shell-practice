ff71dd0c2832a62caa739152fb7440f4dfbc37ae
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-12 16:39:19 +0530

    variables updated

--- 09-array.sh
+++ 09-array.sh
@@ -3,4 +3,5 @@
 MOVIES=("Devara" "Pushpa2" "Kalki2898AD" "Dragon")
 
 echo "First Movie: ${MOVIES[0]}"
-echo "Second Movie: ${MOVIES[3]}" 
+echo "Second Movie: ${MOVIES[3]}"
+ echo "All Movies: ${MOVIES[@]}"
