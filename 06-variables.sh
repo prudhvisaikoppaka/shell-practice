fe4add67e7828e3221109e174024ee428558caaf
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-12 15:09:28 +0530

    variables updated

---
+++ 06-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2 Anna, How are you?"
+echo "$PERSON2:: Hi ra, I am Good. How are you doing"
+echo "$PERSON1:: I am fine too. What about Dragon?"
+echo "$PERSON2:: Dragon coming excellent."
+echo "$PERSON1:: Can't wait to see Dragon anna."
+echo "$PERSON2:: It'll be blast!"
+echo "$PERSON1:: JAI NTR!"
