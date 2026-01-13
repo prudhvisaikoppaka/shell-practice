549324a5892a9389ef42d8775ae40383d880d77b
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-13 17:39:00 +0530

    special vars

--- 11-condition.sh
+++ 11-condition.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-NUMBER=$9
+NUMBER=$99
 
 # -gt --> greater than
 # -lt --> less than
 # -eq --> equal
 # -ne --> not equal
 
-if [ $NUMBER 9 -lt 10 ]
-then
-   echo "Given number $NUMBER is less than 10"
-else
-   echo "Given number $NUMBER is not less than 10"
-fi
-
-
-NUMBER=$99
-
 if [ $NUMBER 99 -gt 10 ]
+then
    echo "Given number $NUMBER is greater than 10"
 else
-   echo "Given number $NUMBER is not greater than 10"   
+   echo "Given number $NUMBER is not greater than 10"
 fi
