01c1db08a9005161313060844952f82f0eb81737
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-13 11:32:13 +0530

    special vars

--- 10-special-vars.sh
+++ 10-special-vars.sh
@@ -4,8 +4,8 @@ echo "All variables passed to the script: $@"
 echo "Number of variables: $#"
 echo "Script name: $0"
 echo "Present working directory: $PWD"
-echo "Home directory of the user: $Home"
 echo "User running this script: $USER"
+echo "Home directory of the user: $Home"
 echo "PID of the current script: $$"
-Sleep 10 &
+sleep 10 &
 echo "PID of the last command running in background: $!"
