b846395ab412f45d4a46db02476eb8c189c84101
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-14 11:22:36 +0530

    condition applied

--- 11-condition.sh
+++ 11-condition.sh
@@ -11,5 +11,5 @@ if [ $NUMBER 99 -gt 10 ]
 then
    echo "Given number $NUMBER is greater than 10"
 else
-   echo "Given number $NUMBER is not greater than 10"
+   echo "Given number $NUMBER is less than 10"
 fi
