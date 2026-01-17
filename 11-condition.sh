4e0b361058f57617e6e2980aed739e635e448c4f
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-17 14:30:59 +0530

    condition applied

--- 11-condition.sh
+++ 11-condition.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-NUMBER=$9
+NUMBER=$1
 
 # -gt --> greater than
 # -lt --> less than
 # -eq --> equal
 # -ne --> not equal
 
-if [ $NUMBER -gt 90 ]
+if [ $NUMBER -gt 10 ]
 then
-   echo "Given number $NUMBER is greater than 90"
+   echo "Given number $NUMBER is greater than 10"
 else
-   echo "Given number $NUMBER is not greater than 90"
+   echo "Given number $NUMBER is not greater than 10"
 fi
