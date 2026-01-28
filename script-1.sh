c490e9f88a1883c4fe6d89e3a640aabd7705d65c
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-28 15:17:41 +0530

    script practice

--- script-1.sh
+++ script-1.sh
@@ -4,6 +4,4 @@ GREETING="Hello, Good Morning"
 echo "$GREETING"
 echo "PID of SCRIPT-1: $$"
 
-# ./script-2.sh # Here "." represents current directory
-
-source ./script-2.sh
+./script-2.sh # Here "." represents current directory
