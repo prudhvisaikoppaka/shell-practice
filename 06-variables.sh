faf7fdfe5024aee9efafdece775751c2068cb76a
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-29 12:49:02 +0530

    shell-practice

--- 06-variables.sh
+++ 06-variables.sh
@@ -3,10 +3,10 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1:: Hi Mr. $PERSON2, How are you?"
-echo "$PERSON2:: Hi Mr.$PERSON1, I am Good. How are you doing"
-echo "$PERSON1:: I am fine too. Using AI is so cool"
-echo "$PERSON2:: Yeah, that's why i created perplexity."
-echo "$PERSON1:: You are doing Great"
-echo "$PERSON2:: Thank You!"
-echo "$PERSON1:: See you later."
+echo "$1:: Hi Mr. $2, How are you?"
+echo "$2:: Hi Mr.$1, I am Good. How are you doing"
+echo "$1:: I am fine too. Using AI is so cool"
+echo "$2:: Yeah, that's why i created perplexity."
+echo "$1:: You are doing Great"
+echo "$2:: Thank You!"
+echo "$1:: See you later."
