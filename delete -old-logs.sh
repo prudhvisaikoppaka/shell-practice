e6afaae71100aa7a345d9c8343240a4f4a9122c2
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-29 16:38:43 +0530

    shell-practice

---
+++ delete -old-logs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SOURCE_DIR=/home/ec2-user/app-logs
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.LOG"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+   echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+   exit 1 #give other than 0 upto 127
+else
+   echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# Validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+  if [ $1 -eq 0 ]
+   then
+      echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+   else
+      echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+      exit 1
+   fi       
+}
+
+FILES_TO_DELETE=$(find . -name "*.log" -mtime +14)
+
+while IFS= read -r filepath
+do
+   echo "Deleting file: $filepath" | tee -a $LOG_FILE
+   rm -rff $filepath
+done <<< $FILES_TO_DELETE
+
+echo "Script executed successfully"
