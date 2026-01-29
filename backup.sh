489709233e99181239418f771105680e96a914df
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-29 21:15:47 +0530

    shell-practice

--- backup.sh
+++ backup.sh
@@ -56,3 +56,10 @@ then
 fi
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
+
+if [ ! -z $FILES ]
+then
+   echo "Files found"
+else
+   echo -e "No log files found older than 14 days ... $Y SKKIPING $N"
+fi      
