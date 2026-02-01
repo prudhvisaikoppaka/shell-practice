3bcaa40d8ff1e8cdd015c6675cc0899617ee03ce
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-02-01 22:43:01 +0530

    backup practice

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ DAYS=${3:-14} # if Days are provided that will be considered, otherwise 14 days 
 
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.LOG"
+LOG_FILE="$LOGS_FOLDER/backup.LOG" #  Use $SCRIPT_NAME in script and use backup in saving command version
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
