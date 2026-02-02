4d6a52e90549db62ca4b322d2920180cdb65b11c
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-02-02 22:58:27 +0530

    shell-practice

--- disk-usage.sh
+++ disk-usage.sh
@@ -3,6 +3,7 @@
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 # in project it will be 75
 MSG=""
+IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read line
 do
@@ -10,8 +11,10 @@ do
   PARTITION=$(echo $line | awk '{print $7F}')
   if [ $USAGE -ge $DISK_THRESHOLD ]
   then
-      MSG+="High Disk Usage on $PARTITION: $USAGE \n" # \n is for New line in shell 
+      MSG+="High Disk Usage on $PARTITION: $USAGE <br>" # \n is for New line in shell # <br> represents HTML new line
   fi
 done <<< $DISK_USAGE
 
 echo -e $MSG # -e for enable new line
+
+sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "prudhvisaikoppaka9989@gmail.com" "ALERT-High Disk Usage"
