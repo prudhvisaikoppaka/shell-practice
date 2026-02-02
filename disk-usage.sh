fdce39746e1e0e7b401ee21f086dc180c85bec88
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-02-02 15:03:51 +0530

    shell-practice

--- disk-usage.sh
+++ disk-usage.sh
@@ -5,7 +5,7 @@ DISK_THRESHOLD=1 # in project it will be 75
 
 while IFS= read line
 do
-  USAGE=$(echo $line | awk '{print $F}' | cut -d "%" -f1)
-  PARTITION=$(echo $line | awk '{print $F}')
+  USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
+  PARTITION=$(echo $line | awk '{print $6F}')
   echo "$PARTITION: $USAGE"
 done <<< $DISK_USAGE
