b18565bdfaabf3970074626bb21f2fa92d99e4ef
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-02-02 23:27:32 +0530

    shell-practice

--- disk-usage.sh
+++ disk-usage.sh
@@ -17,4 +17,4 @@ done <<< $DISK_USAGE
 
 #echo -e $MSG  -e for enable new line
 
-sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "prudhvisaikoppaka9989@gmail.com" "ALERT-High Disk Usage"
+sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "srinaveen1101@gmail.com" "ALERT-High Disk Usage"
