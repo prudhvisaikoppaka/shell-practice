21eeb105e4ef923dd49ae95a5aaf69faf01f5f84
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-02-02 23:36:07 +0530

    shell-practice

--- cpu-usage.sh
+++ cpu-usage.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # Get CPU usage percentage
 
-while true; do
-    cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
-    echo "CPU Usage: ${cpu_usage}%" 
-    sleep 1
-done    
+
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+echo "CPU Usage: ${cpu_usage}%"   
 # Alternative using mpstat
 # mpstat 1 1 | awk '/Average/ {print 100 - $NF"%"}'
